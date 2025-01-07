d641e0643926341b7aa5c47c29bd371e08ea5b1d
qpham4243562 <139883097+qpham4243562@users.noreply.github.com>
2025-01-07 13:55:04 +0700

    Update update-user.dto.ts

--- src/models/user/dto/update-user.dto.ts
+++ src/models/user/dto/update-user.dto.ts
@@ -1,65 +1,30 @@
 import {
-  IsString,
   IsEmail,
   IsOptional,
-  IsEnum,
-  IsBoolean,
-  IsDate,
+  IsNotEmpty,
 } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
-import { UserRole } from '../entity/user.entity';
+
 
 export class UpdateUserDto {
-  @ApiProperty({ description: 'The name of the user', required: false })
-  @IsOptional()
-  @IsString()
-  name?: string;
-
-  @ApiProperty({ description: 'The email of the user', required: false })
-  @IsOptional()
-  @IsEmail()
-  email?: string;
-
-  // @ApiProperty({ description: 'The phone number of the user', required: false })
-  // @IsOptional()
-  // @IsString()
-  // phone?: string;
-
-  // @ApiProperty({
-  //   description: 'Indicates whether the user is active',
-  //   required: false,
-  // })
-  // @IsOptional()
-  // @IsBoolean()
-  // isActive?: boolean;
-
-  // @ApiProperty({
-  //   description: 'The role of the user',
-  //   enum: UserRole,
-  //   required: false,
-  // })
-  // @IsOptional()
-  // @IsEnum(UserRole)
-  // role?: UserRole;
-
-  @ApiProperty({
-    description: 'The avatar of the user',
-    type: 'string',
-    format: 'binary',
-    required: false,
-  })
-  @IsOptional()
-  avatar?: string;
-
-  // @ApiProperty({
-  //   description: 'The date of birth of the user',
-  //   required: false,
-  // })
-  // @IsOptional()
-  // @IsDate()
-  // dateOfBirth?: Date;
-  // @ApiProperty({ description: 'The address of the user', required: false })
-  // @IsOptional()
-  // @IsString()
-  // address?: string;
+  @ApiProperty({ description: 'User name', example: 'John Doe' })
+     @IsNotEmpty()
+     name: string;
+ 
+     @ApiProperty({ description: 'User phone number', example: '123456789' })
+     @IsOptional()
+     phone: string;
+ 
+     @ApiProperty({ description: 'User address', example: '123 Main Street' })
+     @IsOptional()
+     address: string;
+ 
+     @ApiProperty({ description: 'User email', example: 'johndoe@example.com' })
+     @IsNotEmpty()
+     @IsEmail()
+     email: string;
+ 
+     @ApiProperty({ description: 'User password', example: 'password123' })
+     @IsNotEmpty()
+     password: string;
 }
